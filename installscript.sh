df6f22a23e92b74ea3b7f3777040b4c09c0b9c4f
Grant Williams <grant@gwscripts.com>
2021-11-02 03:22:44 -0500

    Clone repositories last

--- installscript.sh
+++ installscript.sh
@@ -56,15 +56,15 @@ cd $HOME/Code
 valet park
 cd $HOME
 
-# Clone Github repositories
-./clone.sh
-
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/shell/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/macos/.mackup.cfg $HOME/.mackup.cfg
+
+# Clone Github repositories
+./clone.sh
 
 echo '++++++++++++++++++++++++++++++'
 echo '++++++++++++++++++++++++++++++'
